431769c138539dd75472060b18410ae8fad26513
Raquelll77 <raquelalvarado1717@gmail.com>
2025-03-05 10:16:47 -0600

    trayendo la respuesta de la api creando la schema y mostrando las categorias en nuestro select

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,6 +7,7 @@ export default function Header() {
     const isHome = useMemo(() => pathname === '/',[pathname])
     
     const fetchCategories = useAppStore((state) =>state.fetchCategories)
+    const categories = useAppStore((state) =>state.categories)
 
     useEffect(()=>{
         fetchCategories()
@@ -40,6 +41,10 @@ export default function Header() {
                         <select id="ingredient" name="ingredient" className='p-3 w-full rounded-lg focus:outline-none bg-white'
                         >
                             <option value="">-- Seleccione --</option>
+                            {categories.drinks.map((category)=>(
+                                <option key={category.strCategory} value={category.strCategory}>{category.strCategory}
+                                </option>
+                            ))}
                         </select>
                         
                     </div>
