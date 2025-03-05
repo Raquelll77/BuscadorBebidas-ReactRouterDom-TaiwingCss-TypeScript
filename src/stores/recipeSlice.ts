614f154e3da41dc5245429f3d3ac72ade55579f2
Raquelll77 <raquelalvarado1717@gmail.com>
2025-03-05 13:49:01 -0600

    consultando api mediante los filtros

--- src/stores/recipeSlice.ts
+++ src/stores/recipeSlice.ts
@@ -1,6 +1,7 @@
 import { StateCreator } from "zustand"
-import { getCategories } from "../services/RecipeService"
+import { getCategories, getRecipes} from "../services/RecipeService"
 import type { Categories, SearchFilter } from "../types"
+
 
 export type RecipesSliceType = {
     categories: Categories
@@ -18,6 +19,6 @@ export const createRecipesSlice :StateCreator<RecipesSliceType>= (set) => ({
        })    
     },
     searchRecipes: async(filters) => {
-        console.log(filters)
+        await getRecipes(filters)
     }
 })
