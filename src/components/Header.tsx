9fae3fd0318cb328103a348eb5ec61df281d8578
Raquelll77 <raquelalvarado1717@gmail.com>
2025-03-05 11:45:55 -0600

    agregando la seleccion del formulario en el state

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,11 @@
-import { useEffect, useMemo } from 'react'
+import { ChangeEvent, useEffect, useMemo, useState } from 'react'
 import {NavLink, useLocation} from 'react-router-dom'
 import { useAppStore } from '../stores/useAppStore'
 export default function Header() {
+    const [searchFilters, setSearchFilters] = useState({
+        ingredient: '',
+        category: ''
+    })
 
     const {pathname} = useLocation()
     const isHome = useMemo(() => pathname === '/',[pathname])
@@ -12,6 +16,13 @@ export default function Header() {
     useEffect(()=>{
         fetchCategories()
     },[])
+
+    const handleChange = (e: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLSelectElement>) => {
+        setSearchFilters({
+            ...searchFilters,
+            [e.target.name] : e.target.value
+        })
+    }
 
   return (
     <header className={isHome ? 'bg-[url(/bg.jpg)] bg-center bg-cover' : 'bg-slate-800'}>
@@ -33,12 +44,12 @@ export default function Header() {
                 <form className='md:w-1/2 2xl:w-1/3 bg-orange-400 my-32 p-10 rounded-lg shadow space-y-6'>
                     <div className="space-y-4">
                         <label htmlFor="ingredient" className='block text-white uppercase font-extrabold text-lg'>Nombre o Ingrediente</label>
-                        <input id="ingredient" type="text" placeholder='Nombre o ingrediente. Ej. Vodka, Tequila Café' name="ingredient" className='p-3 w-full rounded-lg focus:outline-none bg-white'/>
+                        <input id="ingredient" type="text" placeholder='Nombre o ingrediente. Ej. Vodka, Tequila Café' name="ingredient" className='p-3 w-full rounded-lg focus:outline-none bg-white' onChange={handleChange} value={searchFilters.ingredient}/>
 
                     </div>
                     <div className="space-y-4">
-                        <label htmlFor="ingredient" className='block text-white uppercase font-extrabold text-lg'>Categoria</label>
-                        <select id="ingredient" name="ingredient" className='p-3 w-full rounded-lg focus:outline-none bg-white'
+                        <label htmlFor="category" className='block text-white uppercase font-extrabold text-lg'>Categoria</label>
+                        <select id="category" name="category" className='p-3 w-full rounded-lg focus:outline-none bg-white' onChange={handleChange} value={searchFilters.ingredient}
                         >
                             <option value="">-- Seleccione --</option>
                             {categories.drinks.map((category)=>(
--- src/views/IndexPage.tsx
+++ src/views/IndexPage.tsx
@@ -2,7 +2,7 @@ export default function IndexPage() {
   
   return (
     <>
-        <h1>Inicio</h1>
+      <h1>Inicio</h1>
     </>
   )
 }
