e51458562f3fa8cbfd7b5e01b907b96549bcc9d6
Raquelll77 <raquelalvarado1717@gmail.com>
2025-03-05 14:39:41 -0600

    centrando texto Recetas

--- src/views/IndexPage.tsx
+++ src/views/IndexPage.tsx
@@ -9,7 +9,7 @@ export default function IndexPage() {
 
   return (
     <>
-      <h1 className="text-6xl font-extrabold">Recetas</h1>
+      <h1 className="text-6xl font-extrabold text-center">Recetas</h1>
       {hasDrinks ? (
         <div className="grid grid-cols-1 md:grid-cols-2 2xl:grid-cols-3 my-10 gap-10">
           {drinks.drinks.map((drink) =>(
