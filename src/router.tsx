bd59448980be2a98944f30312cd5e2108621c733
Raquelll77 <raquelalvarado1717@gmail.com>
2025-03-07 16:03:21 -0600

    agregando performance a index.js

--- src/router.tsx
+++ src/router.tsx
@@ -1,15 +1,18 @@
 import {lazy, Suspense} from 'react'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import IndexPage from './views/IndexPage'
 import Layout from './layouts/Layout'
 const FavoritesPage = lazy(() =>import('./views/FavoritesPage'))
-
+const IndexPage = lazy(() =>import('./views/IndexPage'))
 export default function AppRouter() {
   return (
     <BrowserRouter>
         <Routes>
             <Route element={<Layout/>}>
-                <Route path='/' element={<IndexPage/>} index/>
+                <Route path='/' element={
+                  <Suspense fallback= "Cargando...">
+                    <IndexPage/>
+                  </Suspense>
+                } index/>
                 <Route path='/favoritos' element={
                   <Suspense fallback= "Cargando...">
                     <FavoritesPage/>
